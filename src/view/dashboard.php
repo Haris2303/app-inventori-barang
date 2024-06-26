650d574b9dd1f074964918061591ac6718954dd1
haris2303 <hrsccf102@gmail.com>
2024-06-26 21:08:52 +0900

    Create a feature categories

--- src/view/dashboard.php
+++ src/view/dashboard.php
@@ -8,6 +8,15 @@ if (!isset($_SESSION['is_login'])) {
     exit;
 }
 
+if (isset($_GET['id'])) {
+    delete($_GET['id']);
+    echo "
+    <script>
+        alert('User berhasil dihapus')
+        document.location.href = './dashboard.php'
+    </script>";
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -20,29 +29,10 @@ if (!isset($_SESSION['is_login'])) {
 </head>
 
 <body>
+
+    <a href="user.php">Users</a>
+    <a href="category.php">Categories</a>
     <a href="../services/logout.php">Logout</a>
-
-    <h1>Data Users</h1>
-
-    <table border="1" cellpadding="10" cellspacing="0">
-        <tr>
-            <th>No</th>
-            <th>Nama</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th>Aksi</th>
-        </tr>
-        <?php $i = 1 ?>
-        <?php foreach (selectAll() as $row) : ?>
-            <tr>
-                <td><?= $i++ ?></td>
-                <td><?= $row['name'] ?></td>
-                <td><?= $row['email'] ?></td>
-                <td><?= $row['roles'] ?></td>
-                <td><a href="#">Edit</a> | <a href="#">Hapus</a></td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
 
 </body>
 
